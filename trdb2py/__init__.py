96873c4a9b170438482661b9918674878973af3a
zhs007 <zerrozhao@gmail.com>
2021-04-25 07:16:30 +0000

    Added showIndicators

--- trdb2py/__init__.py
+++ trdb2py/__init__.py
@@ -16,6 +16,6 @@ from trdb2py.plotly import (showAssetCandles, showHeatmap, showHeatmapWinRateInY
                             showWinRateInYears, showHeatmapWinRateInMonths, showWinRateInMonths,
                             showBarWinRateInMonths, showBarWinRateInYears, showBarWinRate4Month,
                             showPNL, showPNLs, showPNLs2, showBarResponseRateInYears,
-                            showAssetCandles2)
+                            showAssetCandles2, showIndicators)
 from trdb2py.trdb2utils import analysisResult, getIndicatorInResult, getFirstCtrlTs, buildPNLDataFrame
 import trdb2py.trading2_pb2
