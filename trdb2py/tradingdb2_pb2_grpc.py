5a003970eb339eefce6da3d8e2b943b45a10c53c
zhs007 <sssxueren@gmail.com>
2021-05-10 09:02:02 +0000

    Updated protos

--- trdb2py/tradingdb2_pb2_grpc.py
+++ trdb2py/tradingdb2_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import trdb2py.tradingdb2_pb2 as tradingdb2__pb2
+import tradingdb2_pb2 as tradingdb2__pb2
 
 
 class TradingDB2Stub(object):
