7ac4402a5d4b22098ddec0ab72bc772a34b169bf
zhs007 <sssxueren@gmail.com>
2021-05-29 15:29:30 +0800

    - Fixed #97

--- trdb2py/assetsutils.py
+++ trdb2py/assetsutils.py
@@ -2,7 +2,7 @@
 import trdb2py.trading2_pb2
 import numpy as np
 from trdb2py.trdb2 import getAssetCandles3
-from trdb2py.candlesutils import calcCandlesSimilarity
+from trdb2py.candlesutils import calcCandlesSimilarity, calcCandlesSimilarity_LowLevel
 
 
 def calcAssetsSimilarity(cfg: dict, asset0: str, asset1: str, tsStart: int, tsEnd: int):
@@ -21,6 +21,30 @@ def calcAssetsSimilarity2(cfg: dict, names: list, assets: list, tsStart: int, ts
                 list.append(1.0)
             else:
                 cs = calcAssetsSimilarity(cfg, asset0, asset1, tsStart, tsEnd)
+                list.append(cs)
+
+        arr.append(list)
+
+    return arr
+
+
+def calcAssetsSimilarity_LowLevel(cfg: dict, asset0: str, asset1: str, tsStart: int, tsEnd: int):
+    candles0 = getAssetCandles3(cfg, asset0, tsStart, tsEnd)
+    candles1 = getAssetCandles3(cfg, asset1, tsStart, tsEnd)
+
+    return calcCandlesSimilarity_LowLevel(candles0, candles1)
+
+
+def calcAssetsSimilarity2_LowLevel(cfg: dict, names: list, assets: list, tsStart: int, tsEnd: int):
+    arr = []
+    for asset0 in assets:
+        list = []
+        for asset1 in assets:
+            if asset0 == asset1:
+                list.append(1.0)
+            else:
+                cs = calcAssetsSimilarity_LowLevel(
+                    cfg, asset0, asset1, tsStart, tsEnd)
                 list.append(cs)
 
         arr.append(list)
--- trdb2py/candlesutils.py
+++ trdb2py/candlesutils.py
@@ -21,3 +21,33 @@ def calcCandlesSimilarity(candles0: trdb2py.trading2_pb2.Candles, candles1: trdb
         arr.append(arr1[i] / arr0[i])
 
     return np.mean(arr)
+
+
+def calcCandlesSimilarity_LowLevel(candles0: trdb2py.trading2_pb2.Candles, candles1: trdb2py.trading2_pb2.Candles):
+    if len(candles0.candles) != len(candles1.candles):
+        raise ValueError
+
+    arr0 = []
+    arr1 = []
+
+    for candle in candles0.candles:
+        if candle.close > candle.open:
+            arr0.append(1.0)
+        elif candle.close < candle.open:
+            arr0.append(-1.0)
+        else:
+            arr0.append(0.0)
+
+    for candle in candles1.candles:
+        if candle.close > candle.open:
+            arr1.append(1.0)
+        elif candle.close < candle.open:
+            arr1.append(-1.0)
+        else:
+            arr1.append(0.0)
+
+    arr = []
+    for i in range(len(arr0)):
+        arr.append(arr1[i] / arr0[i])
+
+    return np.mean(arr)
