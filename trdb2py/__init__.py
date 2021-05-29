b17822b08162eb5347535867b26bf57fb75eabe9
zhs007 <sssxueren@gmail.com>
2021-05-29 13:07:30 +0800

    - Fixed #91

--- trdb2py/__init__.py
+++ trdb2py/__init__.py
@@ -5,6 +5,7 @@ from trdb2py.trdb2 import (simTradings, getAssetCandles,
                            simTrading, getAssetCandles2, simTradings3, getAssetCandles3)
 from trdb2py.utils import str2asset, nextWeekDay, asset2str
 from trdb2py.candlesutils import calcCandlesSimilarity
+from trdb2py.assetsutils import calcAssetsSimilarity
 from trdb2py.timeutils import str2timestamp, getDayInYear, getYearDays, calcYears
 from trdb2py.pdutils import (buildPNLReport, getPNLLastTs, getPNLValueWithTimestamp, mergePNL, mergePNLEx,
                              rmPNLValuesWithTimestamp, getPNLTimestampLowInMonth, getPNLTimestampHighInMonth,
