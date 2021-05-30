721c4f4c1ae376f8c0b1bed025bd81d2ec8712a8
zhs007 <sssxueren@gmail.com>
2021-05-30 13:48:52 +0000

    Added calcCandlesSimilarity_LowLevel2

--- trdb2py/__init__.py
+++ trdb2py/__init__.py
@@ -4,9 +4,10 @@ from trdb2py.indicator import isPriceIndicator, isNeedSecondY
 from trdb2py.trdb2 import (simTradings, getAssetCandles,
                            simTrading, getAssetCandles2, simTradings3, getAssetCandles3)
 from trdb2py.utils import str2asset, nextWeekDay, asset2str
-from trdb2py.candlesutils import calcCandlesSimilarity, calcCandlesSimilarity_LowLevel
+from trdb2py.candlesutils import (calcCandlesSimilarity, calcCandlesSimilarity_LowLevel, calcCandlesSimilarity_LowLevel2)
 from trdb2py.assetsutils import (calcAssetsSimilarity, calcAssetsSimilarity2,
-                                 calcAssetsSimilarity_LowLevel, calcAssetsSimilarity2_LowLevel)
+                                 calcAssetsSimilarity_LowLevel, calcAssetsSimilarity2_LowLevel,
+                                 calcAssetsSimilarity_LowLevel2, calcAssetsSimilarity2_LowLevel2)
 from trdb2py.timeutils import str2timestamp, getDayInYear, getYearDays, calcYears
 from trdb2py.pdutils import (buildPNLReport, getPNLLastTs, getPNLValueWithTimestamp, mergePNL, mergePNLEx,
                              rmPNLValuesWithTimestamp, getPNLTimestampLowInMonth, getPNLTimestampHighInMonth,
--- trdb2py/candlesutils.py
+++ trdb2py/candlesutils.py
@@ -52,3 +52,57 @@ def calcCandlesSimilarity_LowLevel(candles0: trdb2py.trading2_pb2.Candles, candl
             nums = nums + 1
 
     return nums / len(arr0)
+
+
+def calcCandlesSimilarity_LowLevel2(candles0: trdb2py.trading2_pb2.Candles, candles1: trdb2py.trading2_pb2.Candles):
+    if len(candles0.candles) != len(candles1.candles):
+        raise ValueError
+
+    arr0 = []
+    arr1 = []
+    arrv0 = []
+    arrv1 = []
+
+    for candle in candles0.candles:
+        arrv0.append(candle.close - candle.open)
+
+        if candle.close > candle.open:
+            arr0.append(1.0)
+        elif candle.close < candle.open:
+            arr0.append(-1.0)
+        else:
+            arr0.append(0.0)
+
+    for candle in candles1.candles:
+        arrv1.append(candle.close - candle.open)        
+
+        if candle.close > candle.open:
+            arr1.append(1.0)
+        elif candle.close < candle.open:
+            arr1.append(-1.0)
+        else:
+            arr1.append(0.0)
+
+    nums = 0
+    cv = 0
+    for i in range(len(arr0)):
+        if (arr0[i] > 0 and arr1[i] >= 0) or (arr1[i] > 0 and arr0[i] >= 0):
+            nums = nums + 1
+
+            if arrv0[i] <= arrv1[i]:
+                cv = cv + 1.0
+            elif arr0[i] == 0:
+                cv = cv + 0.0
+            else:
+                cv = cv + arrv1[i] / arrv0[i]
+        elif (arr0[i] < 0 and arr1[i] <= 0) or (arr1[i] < 0 and arr0[i] <= 0):
+            nums = nums + 1
+
+            if arrv0[i] >= arrv1[i]:
+                cv = cv + 1.0
+            elif arr0[i] == 0:
+                cv = cv + 0.0                
+            else:
+                cv = cv + arrv1[i] / arrv0[i]            
+
+    return (nums / len(arr0)) * (cv / nums)
